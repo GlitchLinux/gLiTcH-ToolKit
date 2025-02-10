954d3a907bbeb400aedbf87a27750d0f4d9280ae
GLITCH LINUX <info@glitchlinux.com>
2025-02-10 09:23:15 +0100

    Update FLATPAK-INSTALL.sh

--- FLATPAK-INSTALL.sh
+++ FLATPAK-INSTALL.sh
@@ -4,10 +4,9 @@
 sudo apt update
 sudo apt install -y flatpak
 
-# Install GNOME Software plugin for Flatpak
-sudo apt install -y gnome-software-plugin-flatpak
-
 # Add Flathub repository if not already added
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 echo "Flatpak installation and configuration completed."
+
+exit
