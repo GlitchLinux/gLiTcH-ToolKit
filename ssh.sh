04b19923e096aac23687f5365f552ef80c2ee208
GLITCH LINUX <info@glitchlinux.com>
2025-02-11 06:55:11 +0100

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -4,13 +4,15 @@
 HOME_SERVER="ssh x@85.226.230.106 -p 2222"
 INT_SERVER_1="ssh grp13-serv1@193.10.236.126"
 INT_SERVER_2="ssh grp13-serv2@193.10.236.127"
+UBUNTU_MINI_SERVER="ssh x@192.168.0.78"
 
 # Display menu
 echo "Select a server to connect to:"
 echo "1) gLiTcH-SERVER"
 echo "2) INT Server 1"
 echo "3) INT Server 2"
-echo "4) Exit"
+echo "4) Ubuntu-Mini-Server"
+echo "5) Exit"
 read -p "Enter your choice: " choice
 
 # Handle user input
@@ -28,6 +30,10 @@ case $choice in
         exec $INT_SERVER_2
         ;;
     4)
+        echo "Connecting to Ubuntu-Mini-Server..."
+        exec $UBUNTU_MINI_SERVER
+        ;;
+    5)
         echo "Exiting..."
         exit 0
         ;;
@@ -36,4 +42,3 @@ case $choice in
         exit 1
         ;;
 esac
-
