a8496748abdc0ae1f35feeeb6feb80a374093c37
GLITCH LINUX <info@glitchlinux.com>
2025-04-10 00:17:28 +0200

    Update fastfetch-install.sh

--- fastfetch-install.sh
+++ fastfetch-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.36.1/fastfetch-linux-amd64.deb
+wget --no-check-certificate https://github.com/fastfetch-cli/fastfetch/releases/download/2.36.1/fastfetch-linux-amd64.deb
 sudo dpkg -i fastfetch-linux-amd64.deb
 fastfetch
