f554f6a63b3c1fffbd03e1218c398a3cff29cd0e
GLITCH LINUX <info@glitchlinux.com>
2025-04-10 02:42:50 +0200

    Update Titus-Bash-Shell.sh

--- Titus-Bash-Shell.sh
+++ Titus-Bash-Shell.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+apt update
+wget --no-check-certificate https://github.com/fastfetch-cli/fastfetch/releases/download/2.36.1/fastfetch-linux-amd64.deb
+sudo dpkg -i fastfetch-linux-amd64.deb
+rm fastfetch-linux-amd64.deb
+
 # Create build directory and clone repo
 mkdir -p ~/build
 cd ~/build
