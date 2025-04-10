e44503f9165d0ef5fd24aade8b9083e03bc028fd
GLITCH LINUX <info@glitchlinux.com>
2025-04-10 05:26:31 +0200

    Delete gLiTcH-Installer.sh

--- gLiTcH-Installer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt update && sudo apt install -y calamares calamares-settings-debian
-sudo rm -r /etc/calamares/branding/debian/
-sudo rm  /etc/calamares/settings.conf
-sudo cp -r /home/x/txt.and.sh/calamares/branding/debian/ /etc/calamares/branding/
-sudo cp -r /home/x/txt.and.sh/calamares/settings.conf /etc/calamares/
-sudo calamares
