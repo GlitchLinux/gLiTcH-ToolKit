eeb7302cab3ffbe5c880a0cdd5fc073db7546c3b
GLITCH LINUX <info@glitchlinux.com>
2025-05-07 23:40:22 +0200

    Update DD-CLI.py

--- DD-CLI.py
+++ DD-CLI.py
@@ -3,4 +3,4 @@ sudo apt update && sudo apt install -y python3 python3-pip git pv dosfstools par
 sudo wget https://github.com/GlitchLinux/dd_py_CLI/releases/download/dd-cli_v0.1/dd-cli_v0.1_amd64.deb
 sudo dpkg -i dd-cli_v0.1_amd64.deb
 sudo rm dd-cli_v0.1_amd64.deb
-echo "Install finished, Run DD to execute"
+bash -i -c "DD"
