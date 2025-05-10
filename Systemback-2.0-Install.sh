4fcc8759ef79d6db9e9a3ade28655947ac9f0b42
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2025-05-10 18:08:49 +0200

    Update Systemback-2.0-Install.sh

--- Systemback-2.0-Install.sh
+++ Systemback-2.0-Install.sh
@@ -6,7 +6,9 @@ sudo mkdir systemback && cd systemback
 
 sudo wget https://github.com/GlitchLinux/systemback-2.0/raw/refs/heads/main/SystemBack_2_Deb+Source.tar && sleep 2
 
-sudo tar xvf SystemBack_2_Deb+Source.tar && sudo apt update
+sudo tar xvf SystemBack_2_Deb+Source.tar
+
+cd Install && sudo apt update
 
 sudo dpkg --force-all -i systemback-cli_2.0_amd64.deb systemback_2.0_amd64.deb  systemback-locales_2.0_all.deb systemback-efiboot-amd64_2.0_all.deb libsystemback_2.0_amd64.deb
 
@@ -17,5 +19,7 @@ sudo dpkg --force-all -i systemback-cli_2.0_amd64.deb systemback_2.0_amd64.deb  
 cd /tmp && sudo rm -r systemback
 
 echo"Systemback 2.0 Have Been Sucessfully Installed!"
+
+sleep 3
 
 cd && exit
