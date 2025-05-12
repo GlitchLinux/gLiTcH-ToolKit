6180b3a312a864be569454b8b172eedd8aa63bff
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2025-05-13 01:29:03 +0200

    Update QEMU-QuickBoot.sh

--- QEMU-QuickBoot.sh
+++ QEMU-QuickBoot.sh
@@ -1,5 +1,8 @@
 sudo apt update
-sudo apt install qemu-system wget qemu-utils qemu-system-gui xdotool ovmf qemu-system zenity orchis-gtk-theme
-git clone https://github.com/GlitchLinux/QEMU-QuickBoot.git
+sudo apt install qemu-system wget qemu-utils qemu-system-gui xdotool ovmf qemu-system zenity orchis-gtk-theme -y
+cd /tmp && git clone https://github.com/GlitchLinux/QEMU-QuickBoot.git
 cd QEMU-QuickBoot
-sudo bash QEMU-QuickBoot.sh
+sudo rm -f /home/x/txt.and.sh/BASH-SCRIPTS/QEMU-QuickBoot-NAS.sh
+sudo cp QEMU-QuickBoot.sh /home/x/txt.and.sh/BASH-SCRIPTS/QEMU-QuickBoot-NAS.sh 
+sudo rm -r /tmp/QEMU-QuickBoot
+sudo bash QEMU-QuickBoot-NAS.sh
