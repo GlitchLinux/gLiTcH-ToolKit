cbc5916e75f8864d6e3ebb041f805607887400a6
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2025-05-28 11:07:02 +0200

    Update purge_grub.sh

--- purge_grub.sh
+++ purge_grub.sh
@@ -7,13 +7,13 @@ sudo apt-get purge --allow-remove-essential -y grub-common:*
 sudo apt-get purge --allow-remove-essential -y grub2-common:*
 
 echo "PURGE COMPLETED NOW REINSTALL GRUB!"
-
+echo ""
 echo "sudo apt install grub-efi #UEFI"
-
+echo ""
 echo "OR ->"
-
+echo ""
 echo "sudo apt install grub-pc  #BIOS"
-
+echo ""
 sleep 60
 
 exit
