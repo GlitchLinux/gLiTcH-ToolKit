4aac0afdf77ec39f230fb8a0ce624afba51537d6
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2025-05-29 00:50:11 +0200

    Update Docker-Remastering.sh

--- Docker-Remastering.sh
+++ Docker-Remastering.sh
@@ -263,7 +263,7 @@ wget https://glitchlinux.wtf/FILES/live-boot-initramfs-tools_20221008~fsr1_all.d
 wget https://glitchlinux.wtf/FILES/live-boot_20221008~fsr1_all.deb
 
 sudo dpkg --force-all -i live-boot_20221008~fsr1_all.deb live-boot-initramfs-tools_20221008~fsr1_all.deb live-config-refracta_0.0.5.deb refractasnapshot-base_10.2.12_all.deb
-sudo apt install -f
+sudo apt install -f -y
 sudo dpkg --configure -a
 
 sudo refractasnapshot
