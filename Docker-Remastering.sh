e9a688113c858d80665ef87e6b33451d70de5ef7
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2025-05-29 14:30:30 +0200

    Update Docker-Remastering.sh

--- Docker-Remastering.sh
+++ Docker-Remastering.sh
@@ -248,10 +248,10 @@ sudo chmod +x apps && sudo chmod 777 apps
 sudo cp apps /home && cp apps /home/x && cp apps /root 
 
 cd /tmp
-wget https://glitchlinux.wtf/FILES/refractasnapshot-base_10.2.12_all.deb
-wget https://glitchlinux.wtf/FILES/live-config-refracta_0.0.5.deb
-wget https://glitchlinux.wtf/FILES/live-boot-initramfs-tools_20221008~fsr1_all.deb
-wget https://glitchlinux.wtf/FILES/live-boot_20221008~fsr1_all.deb
+wget https://github.com/GlitchLinux/docker-remastering/raw/refs/heads/main/REFRACTA-ALL-TOOLS_DEB_FILES/refractasnapshot-base_10.2.12_all.deb
+wget https://github.com/GlitchLinux/docker-remastering/blob/main/REFRACTA-ALL-TOOLS_DEB_FILES/live-config-refracta_0.0.5.deb
+wget https://github.com/GlitchLinux/docker-remastering/blob/main/REFRACTA-ALL-TOOLS_DEB_FILES/live-boot-initramfs-tools_20221008~fsr1_all.deb
+wget https://github.com/GlitchLinux/docker-remastering/blob/main/REFRACTA-ALL-TOOLS_DEB_FILES/live-boot-doc_20221008~fsr1_all.deb
 wget https://github.com/GlitchLinux/docker-remastering/raw/refs/heads/main/grub-efi-amd64-signed_1+2.06+13+deb12u1_amd64.deb
 
 sudo dpkg --force-all -i live-boot_20221008~fsr1_all.deb live-boot-initramfs-tools_20221008~fsr1_all.deb live-config-refracta_0.0.5.deb refractasnapshot-base_10.2.12_all.deb grub-efi-amd64-signed_1+2.06+13+deb12u1_amd64.deb
