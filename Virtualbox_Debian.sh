ad806069bb626dc57e16b604a920384a7a9b9f63
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2025-06-27 00:15:13 +0200

    Update Virtualbox_Debian.sh

--- Virtualbox_Debian.sh
+++ Virtualbox_Debian.sh
@@ -5,6 +5,6 @@ wget https://download.virtualbox.org/virtualbox/7.1.6/virtualbox-7.1_7.1.6-16708
 wget https://download.virtualbox.org/virtualbox/7.1.6/Oracle_VirtualBox_Extension_Pack-7.1.6.vbox-extpack
 sudo dpkg -i virtualbox-7.1_7.1.6-167084~Debian~bookworm_amd64.deb
 sudo apt install -f
-sudo apt install linux-headers-$(uname -r)
+sudo apt install linux-headers-amd64
 sudo '/sbin/vboxconfig'
 sudo adduser $USER vboxusers
