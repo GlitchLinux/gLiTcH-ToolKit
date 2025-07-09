61ee165aa87b2bfcf85b1379180cd62c888367eb
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2025-07-09 20:48:17 +0200

    Update thorium-browser-apt-install.sh

--- thorium-browser-apt-install.sh
+++ thorium-browser-apt-install.sh
@@ -4,20 +4,20 @@ sudo mv thorium.list /etc/apt/sources.list.d/
 sudo apt update
 sudo apt install thorium-browser -y
 
-echo "[Desktop Entry]" > /home/x/Desktop/Thorium
-echo "Categories=Internet;" >> /home/x/Desktop/Thorium
-echo "Comment[en_US]=Thorium Browser" >> /home/x/Desktop/Thorium
-echo "Exec=/usr/bin/thorium-browser" >> /home/x/Desktop/Thorium
-echo "Icon=/usr/share/pixmaps/thorium_greyscale_1024.png" >> /home/x/Desktop/Thorium
-echo "Keywords=browser;internet" >> /home/x/Desktop/Thorium
-echo "Name[en_US]=Thorium" >> /home/x/Desktop/Thorium
-echo "Name=Thorium" >> /home/x/Desktop/Thorium
-echo "StartupNotify=false" >> /home/x/Desktop/Thorium
-echo "Terminal=false" >> /home/x/Desktop/Thorium
-echo "Type=Application" >> /home/x/Desktop/Thorium
-echo "Version=1.0" >> /home/x/Desktop/Thorium
+echo "[Desktop Entry]" > /home/$USER/Desktop/Thorium
+echo "Categories=Internet;" >> /home/$USER/Desktop/Thorium
+echo "Comment[en_US]=Thorium Browser" >> /home/$USER/Desktop/Thorium
+echo "Exec=/usr/bin/thorium-browser" >> /home/$USER/Desktop/Thorium
+echo "Icon=/usr/share/pixmaps/thorium_greyscale_1024.png" >> /home/$USER/Desktop/Thorium
+echo "Keywords=browser;internet" >> /home/$USER/Desktop/Thorium
+echo "Name[en_US]=Thorium" >> /home/$USER/Desktop/Thorium
+echo "Name=Thorium" >> /home/$USER/Desktop/Thorium
+echo "StartupNotify=false" >> /home/$USER/Desktop/Thorium
+echo "Terminal=false" >> /home/$USER/Desktop/Thorium
+echo "Type=Application" >> /home/$USER/Desktop/Thorium
+echo "Version=1.0" >> /home/$USER/Desktop/Thorium
 
-sudo chmod +x /home/puppy/Desktop/Thorium
+sudo chmod +x /home/$USER/Desktop/Thorium
 
 sleep 15
 
