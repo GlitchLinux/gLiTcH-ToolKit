121e81b998d9213dbdd64af3bed14c4ae0272cd0
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2025-07-26 14:27:25 +0200

    Update Tor-Install-Onion-APT.sh

--- Tor-Install-Onion-APT.sh
+++ Tor-Install-Onion-APT.sh
@@ -15,7 +15,11 @@ sudo mv /etc/apt/sources.list /tmp/sources.list
 
 sudo wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --dearmor | sudo tee /usr/share/keyrings/deb.torproject.org-keyring.gpg >/dev/null
 
-sudo apt update && sudo apt install tor deb.torproject.org-keyring torbrowser-launcher -y
+sudo apt update && sudo apt install tor deb.torproject.org-keyring -y
+
+cd /tmp && sudo wget http://http.us.debian.org/debian/pool/contrib/t/torbrowser-launcher/
+sudo dpkg -i torbrowser-launcher_0.3.7-3_amd64.deb && sudo apt install -f -y
+sudo dpkg -i torbrowser-launcher_0.3.7-3_amd64.deb
 
 sudo xterm -e 'echo "#deb [signed-by=/usr/share/keyrings/deb.torproject.org-keyring.gpg] https://deb.torproject.org/torproject.org bookworm main" > /etc/apt/sources.list.d/tor.list'
 sudo xterm -e 'echo "#deb-src [signed-by=/usr/share/keyrings/deb.torproject.org-keyring.gpg] https://deb.torproject.org/torproject.org bookworm main" >> /etc/apt/sources.list.d/tor.list'
