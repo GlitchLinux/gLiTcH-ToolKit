e885ea2c096d7700c88e5f4c45d075200ea51b44
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2025-08-10 01:11:39 +0200

    Update Virtualbox_Ubuntu.sh

--- Virtualbox_Ubuntu.sh
+++ Virtualbox_Ubuntu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /tmp
 sudo apt update && sudo apt --reinstall install gcc-12 cpp-12
-wget https://download.virtualbox.org/virtualbox/7.1.12/virtualbox-7.1_7.1.12-169651~Ubuntu~oracular_amd64.deb
-sudo dpkg --force-all -i virtualbox-7.1_7.1.12-169651~Ubuntu~oracular_amd64.deb
+wget https://download.virtualbox.org/virtualbox/7.1.12/virtualbox-7.1_7.1.12-169651~Ubuntu~noble_amd64.deb
+sudo dpkg --force-all -i virtualbox-7.1_7.1.12-169651~Ubuntu~noble_amd64.deb
 sudo apt install -f
-sudo apt install linux-headers-amd64
+sudo apt install linux-headers-amd64 -y
 sudo '/sbin/vboxconfig'
 sudo adduser $USER vboxusers
 cd /home/x/Desktop && wget https://download.virtualbox.org/virtualbox/7.1.6/Oracle_VirtualBox_Extension_Pack-7.1.6.vbox-extpack
