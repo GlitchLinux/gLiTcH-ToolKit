d9b226b03d8f5d835d4f9b32b52def4cd9a55897
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2025-08-09 21:59:29 +0200

    Update DD-GUI.py

--- DD-GUI.py
+++ DD-GUI.py
@@ -1,4 +1,7 @@
-git clone https://github.com/GlitchLinux/dd_py_GUI.git
-cd dd_py_GUI
-sudo apt install python3-tk zenity
-python3 DD-GUI.py
+sudo apt update && sudo apt install python3-tk zenity -y
+cd /tmp
+wget https://github.com/GlitchLinux/dd_py_GUI/releases/download/dd_gui_amd64_v1.1/dd_gui_amd64_v1.1.deb
+sudo dpkg --force-all -i dd_gui_amd64_v1.1.deb 
+sudo apt install -f
+python3 /etc/dd_gui/DD-GUI.py
+exit
