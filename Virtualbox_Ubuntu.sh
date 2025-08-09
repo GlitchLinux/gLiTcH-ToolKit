80defc7334a9b48b8f0f2975775935c2c61b9da1
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2025-08-10 01:13:12 +0200

    Update Virtualbox_Ubuntu.sh

--- Virtualbox_Ubuntu.sh
+++ Virtualbox_Ubuntu.sh
@@ -3,7 +3,7 @@ cd /tmp
 sudo apt update && sudo apt --reinstall install gcc-12 cpp-12
 wget https://download.virtualbox.org/virtualbox/7.1.12/virtualbox-7.1_7.1.12-169651~Ubuntu~noble_amd64.deb
 sudo dpkg --force-all -i virtualbox-7.1_7.1.12-169651~Ubuntu~noble_amd64.deb
-sudo apt install -f
+sudo apt install -f -y
 sudo apt install linux-headers-amd64 -y
 sudo '/sbin/vboxconfig'
 sudo adduser $USER vboxusers
