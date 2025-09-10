736f986637e6661ea60cb3a8d0052805c4cdd20d
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2025-09-10 11:29:21 +0200

    Update tar.sh

--- tar.sh
+++ tar.sh
@@ -1,1 +1,1 @@
-sudo tar --exclude=/tmp --exclude=/mnt --exclude=/media --exclude=/proc/* --exclude=/lost+found --exclude=/swapfile --exclude=/var/cache/apt/archives --exclude=/MiniDeb.tar -C / -c . > ~/MiniDeb.tar
+sudo tar --exclude=/tmp --exclude=/mnt --exclude=/media --exclude=/proc/* --exclude=/lost+found --exclude=/swapfile --exclude=/var/cache/apt/archives --exclude=/MiniDeb.tar -C / -c . > /MiniDeb.tar
