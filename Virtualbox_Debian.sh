1765b3bf281b44741af900020ffaa21931127fe4
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2026-01-06 21:31:14 +0100

    Update Virtualbox_Debian.sh

--- Virtualbox_Debian.sh
+++ Virtualbox_Debian.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /tmp
 sudo apt update && sudo apt --reinstall install gcc-12 cpp-12
-wget https://download.virtualbox.org/virtualbox/7.1.6/virtualbox-7.1_7.1.6-167084~Debian~bookworm_amd64.deb
-wget https://download.virtualbox.org/virtualbox/7.1.6/Oracle_VirtualBox_Extension_Pack-7.1.6.vbox-extpack
-sudo dpkg -i virtualbox-7.1_7.1.6-167084~Debian~bookworm_amd64.deb
+wget https://download.virtualbox.org/virtualbox/7.2.4/virtualbox-7.2_7.2.4-170995~Debian~bookworm_amd64.deb
+wget https://download.virtualbox.org/virtualbox/7.2.4/Oracle_VirtualBox_Extension_Pack-7.2.4.vbox-extpack
+sudo dpkg -i virtualbox-7.2_7.2.4-170995~Debian~bookworm_amd64.deb
 sudo apt install -f
-sudo apt install linux-headers-amd64
+sudo apt install linux-headers-amd64 -y
 sudo '/sbin/vboxconfig'
 sudo adduser $USER vboxusers
