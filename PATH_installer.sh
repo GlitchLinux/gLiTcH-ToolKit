32111af125e1ba1a2169e4f5cd9e6505d599a197
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2026-02-08 05:16:26 +0100

    Update PATH_installer.sh

--- PATH_installer.sh
+++ PATH_installer.sh
@@ -5,8 +5,12 @@ sudo rm -f PATH
 sudo wget https://raw.githubusercontent.com/GlitchLinux/PATH/refs/heads/main/PATH 
 sudo chmod +x PATH
 sudo chmod 777 PATH
+clear
 echo ""
-echo "PATH Utility Installed to system"
-echo ""
-sleep 8
+echo "PATH was Installed to system"
+echo "============================"
+echo "PATH -- Displays full paths "
+echo "Usage: PATH [--list] <target>"
+echo "============================"
+sleep 15
 exit
