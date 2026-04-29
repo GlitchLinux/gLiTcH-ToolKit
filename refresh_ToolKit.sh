53d3d7ed98e5c274395e75aa2cba26cc5e3fc105
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2026-04-29 17:00:40 +0200

    Update refresh_ToolKit.sh

--- refresh_ToolKit.sh
+++ refresh_ToolKit.sh
@@ -1,4 +1,4 @@
 sudo rm -r /tmp/gLiTcH-ToolKit/
 cd /tmp
-sudo wget https://raw.githubusercontent.com/GlitchLinux/gLiTcH-ToolKit/refs/heads/main/apps
-sudo bash apps
+wget https://raw.githubusercontent.com/GlitchLinux/gLiTcH-ToolKit/refs/heads/main/apps
+chmod 777 apps && bash apps
