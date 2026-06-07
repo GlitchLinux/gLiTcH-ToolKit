e67bc09eab5e5310f2803aa72e5bec61d955025f
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2026-06-07 07:46:08 +0200

    Update Glitch-CLI-Wallpaper.sh

--- Glitch-CLI-Wallpaper.sh
+++ Glitch-CLI-Wallpaper.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-cd /tmp
+cd /tmp 
+sudo rm -rf Glitch-CLI-Wallpaper.sh
 wget https://raw.githubusercontent.com/GlitchLinux/Glitch-Plymouth/refs/heads/main/Glitch-Ksplash/Glitch-CLI-Wallpaper.sh
 sudo cp Glitch-CLI-Wallpaper.sh /usr/local/bin/Glitch-CLI-Wallpaper.sh
 sudo chmod +x /usr/local/bin/Glitch-CLI-Wallpaper.sh
 sudo chmod 777 /usr/local/bin/Glitch-CLI-Wallpaper.sh
 
-cat > ~/.config/autostart/Glitch-CLI-Wallpaper.sh << 'EOF'
+mkdir -p /home/x/.config/autostart/
+
+cat > /home/x/.config/autostart/glitch-wallpaper-autostart.sh << 'EOF'
 [Desktop Entry]
 Type=Application
-Name=Glitch-CLI-Wallpaper
+Name=Glitch-Wallpaper-Launcher
 Exec=/usr/local/bin/Glitch-CLI-Wallpaper.sh
 Hidden=false
 EOF
+
+echo ' '
+echo 'Dynamic Wallpaper auto-launcher was successfully installed!' | borderize 
+echo ' '
