476410cf3edb3658e11c69ecef88f288d0fda312
GlitchLinux <info@glitchlinux.com>
2026-06-24 09:05:06 +0200

    Glitch-v33 live downloader

---
+++ Glitch-v33-Live-Download.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+sudo rm -f vmlinuz* filesystem.squashfs* initrd.img* wget-log
+
+sudo rm -f /tmp/.glitch-live.sh
+
+set PWD=pwd
+echo "tree $PWD" > /tmp/path
+
+cat > "/tmp/.glitch-live.sh" << 'EOF'
+
+echo "Downloading Glitch-Linux Live v33 - initrd.img" | borderize
+echo ""
+sudo wget -q --show-progress "https://glitchlinux.wtf/ipxe/Glitch-Linux-v33/live/initrd.img"
+clear
+echo "Downloading Glitch-Linux Live v33 - vmlinuz" | borderize
+echo ""
+sudo wget -q --show-progress "https://glitchlinux.wtf/ipxe/Glitch-Linux-v33/live/vmlinuz"
+clear
+echo "Downloading Glitch-Linux Live v33 - filesystem.squashfs" | borderize
+echo ""
+sudo wget -q --show-progress "https://glitchlinux.wtf/ipxe/Glitch-Linux-v33/live/filesystem.squashfs"
+
+echo " Downloaded Files: " | borderize > /tmp/wget-result
+echo "" >> /tmp/wget-result
+mv Glitch-Linux-Download.sh /tmp/
+rm -f .ventoyignore
+bash /tmp/path >> /tmp/wget-result
+cp /tmp/glitch-live.sh .
+touch .ventoyignore
+clear
+echo "" 
+echo "" >> /tmp/wget-result
+echo "Glitch Linux v33 Sucessfully Downloaded!" >> /tmp/wget-result
+echo "" >> /tmp/wget-result
+
+xterm -geometry 47x17 -e "cat /tmp/wget-result && sleep 120"
+
+EOF
+
+xterm -geometry 60x5 -e 'sudo bash /tmp/.glitch-live.sh' 2>&1 &
+sleep 0.5 && sudo pkill xfce4-terminal
+exit
