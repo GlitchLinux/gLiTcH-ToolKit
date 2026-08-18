2047af96290aa17fee270738fa7709ba20eaa4c7
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2026-08-18 08:31:32 +0200

    Add -y flag to apt reinstall command
    Added '-y' flag to the apt reinstall command for non-interactive installation.

--- Virtualbox_Debian.sh
+++ Virtualbox_Debian.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /tmp
-sudo apt update && sudo apt --reinstall install gcc-12 cpp-12
+sudo apt update && sudo apt --reinstall install gcc-12 cpp-12 -y
 wget https://download.virtualbox.org/virtualbox/7.2.14/virtualbox-7.2_7.2.14-174565~Debian~trixie_amd64.deb
 wget https://download.virtualbox.org/virtualbox/7.2.14/Oracle_VirtualBox_Extension_Pack-7.2.14.vbox-extpack
 sudo dpkg -i virtualbox-7.2_7.2.14-174565~Debian~trixie_amd64.deb
