73944cf67918384525f453d558f8fb2d8cd7e94f
Glitch Linux <154828412+GlitchLinux@users.noreply.github.com>
2026-09-12 01:47:07 +0200

    Update Brave-Browser-Install.sh

--- Brave-Browser-Install.sh
+++ Brave-Browser-Install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt install curl -y
 
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-nightly-archive-keyring.gpg https://brave-browser-apt-nightly.s3.brave.com/brave-browser-nightly-archive-keyring.gpg
@@ -6,4 +8,10 @@ sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-nightly.sources https://b
 
 sudo apt update
 
-sudo apt install brave-origin-nightly
+sudo apt install brave-origin -y
+
+xterm -e 'brave-origin' &
+
+sleep 1 && sudo pkill xterm
+
+exit
