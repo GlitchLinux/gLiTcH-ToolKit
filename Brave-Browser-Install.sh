7215877966d9d3fde9cd86b3911de5779be09630
Glitch Linux <154828412+GlitchLinux@users.noreply.github.com>
2026-09-12 02:52:06 +0200

    Update Brave browser installer for nightly version

--- Brave-Browser-Install.sh
+++ Brave-Browser-Install.sh
@@ -1,17 +1,51 @@
 #!/bin/bash
 
+echo "Brave-Origin-Nightly - Installer" > /tmp/brave-banner
+
+clear
+
+cat /tmp/brave-banner | borderize | lolcat
+
 sudo apt install curl -y
+
+clear
+
+cat /tmp/brave-banner | borderize | lolcat
 
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-nightly-archive-keyring.gpg https://brave-browser-apt-nightly.s3.brave.com/brave-browser-nightly-archive-keyring.gpg
 
+clear
+
+cat /tmp/brave-banner | borderize | lolcat
+
 sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-nightly.sources https://brave-browser-apt-nightly.s3.brave.com/brave-browser.sources
+
+clear
+
+cat /tmp/brave-banner | borderize | lolcat
 
 sudo apt update
 
-sudo apt install brave-origin -y
+clear
+
+cat /tmp/brave-banner | borderize | lolcat
+
+sudo apt install brave-origin-nightly -y
+
+clear
+
+cat /tmp/brave-banner | borderize | lolcat
 
 xterm -e 'brave-origin' &
 
 sleep 1 && sudo pkill xterm
 
-exit
+clear
+
+echo "Brave-Origin-Nightly - Installed to System!" > /tmp/brave-banner
+
+cat /tmp/brave-banner | borderize | lolcat
+
+echo " " 
+
+read p ' '
